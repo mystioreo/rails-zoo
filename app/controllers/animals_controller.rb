0db841255a1bb3286b71cf4b1250102bd2360234
Daniela Di Iorio <danielad@gmail.com>
2018-09-27 13:14:06 -0700

    index, show, new, create, edit, update

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,44 @@
+class AnimalsController < ApplicationController
+  def index
+    @animals = Animal.all
+  end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    if Animal.create(animal_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+
+private
+
+  def animal_params
+    return params.require(:animal).permit(:name, :species, :age)
+  end
+
+
+
+end
